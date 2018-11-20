35d2296a615a94b283ad43440ac3abd47f55f171
Gregory Sims <briggybros.2@googlemail.com>
2018-11-20 16:12:45 +0000

    Add social buttons

--- android/app/src/main/java/teamrocket/amulink/Navigation.java
+++ android/app/src/main/java/teamrocket/amulink/Navigation.java
@@ -5,26 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class Navigation extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_me:
-                    mTextMessage.setText(R.string.title_me);
-                    return true;
-                case R.id.navigation_connected:
-                    mTextMessage.setText(R.string.title_connected);
-                    return true;
-            }
-            return false;
+            return true;
         }
     };
 
@@ -33,8 +22,7 @@ public class Navigation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
