16d6232aa43d6ce951c02af1723e258a1a5915b9
Gregory Sims <briggybros.2@googlemail.com>
2018-11-27 18:38:15 +0000

    Remove circling

--- web-app/src/components/SocialIcon.tsx
+++ web-app/src/components/SocialIcon.tsx
@@ -2,39 +2,25 @@ import * as React from 'react';
 // @ts-ignore
 import simpleIcons from 'simple-icons';
 
-import { Avatar, SvgIcon } from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
 import { Link } from '@material-ui/icons';
 
 interface Props {
   type: string;
-  circled?: boolean;
 }
 
-export default ({ type, circled }: Props) => {
+export default ({ type }: Props) => {
   const icon = simpleIcons[type];
 
   return icon ? (
-    !!circled ? (
-      <Avatar style={{ backgroundColor: `#${icon.hex}` }}>
-        <i
-          dangerouslySetInnerHTML={{ __html: icon.svg }}
-          style={{
-            width: '24px',
-            height: '24px',
-            fill: 'white',
-          }}
-        />
-      </Avatar>
-    ) : (
-      <i
-        dangerouslySetInnerHTML={{ __html: icon.svg }}
-        style={{
-          width: '40px',
-          height: '40px',
-          fill: `#${icon.hex}`,
-        }}
-      />
-    )
+    <i
+      dangerouslySetInnerHTML={{ __html: icon.svg }}
+      style={{
+        width: '40px',
+        height: '40px',
+        fill: `#${icon.hex}`,
+      }}
+    />
   ) : (
     <Avatar>
       <Link />
