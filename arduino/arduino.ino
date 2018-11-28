8f77462102d28e632695863ddfc3d04dee06002a
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-11-28 12:51:30 +0000

    Add drawing name to screen

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,19 +1,47 @@
 /*
 
 */
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
+
+#define TFT_CS 10
+#define TFT_DC 9
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+#define FRAME_X 210
+#define FRAME_Y 180
+#define FRAME_W 100
+#define FRAME_H 50
 
 SoftwareSerial mySerial(3, 2); // RX, TX
 
 String inputString = "";
 DynamicJsonBuffer jsonBuffer(200);
 
-void setup() {
- Serial.begin(9600);
- mySerial.begin(9600);
+void clearScreen() {
+  tft.fillScreen(ILI9341_BLACK);
+}
 
- Serial.println("Hello world");
+void drawText(String text) {
+  tft.setCursor(0 , tft.height() / 2);
+  tft.setTextColor(ILI9341_GREEN);
+  tft.setTextSize(3);
+  tft.println(text);
+}
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  tft.begin();
+  
+  Serial.println("Hello world");
+  
+  clearScreen();
+  // origin = left,top landscape (USB left upper)
+  tft.setRotation(1);
+  drawText("Henlo Lizer");
 }
 
 void loop() {
@@ -26,7 +54,8 @@ void loop() {
       Serial.println("Parsing failed :(");
     } else {
       const String name = root["name"];
-      Serial.println(name);
+      clearScreen();
+      drawText(name);
     }
   }
 }
